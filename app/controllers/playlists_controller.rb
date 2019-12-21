b050e06e733d2adb2572c751522f22ddb88b0ffa
heezhan <hhan1194@gmail.com>
2019-12-21 17:50:42 -0500

    serializations

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,6 +2,14 @@ class PlaylistsController < ApplicationController
     def playlist
         playlist = Playlist.create(user_id: params[:user_id], title: params[:title]) 
 
-        render json: playlist
+        render json: playlist.to_json(serialize)
     end 
+
+    private
+
+    def serialize
+        {
+            :include => [:playlist_episodes, :episodes]
+        }
+    end
 end
