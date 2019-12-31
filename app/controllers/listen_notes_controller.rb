5c588d25a6c7aaf89813bb8dc9a3637001260f32
heezhan <hhan1194@gmail.com>
2019-12-31 13:17:18 -0500

    minimum viable product

--- app/controllers/listen_notes_controller.rb
+++ app/controllers/listen_notes_controller.rb
@@ -11,6 +11,38 @@ class ListenNotesController < ApplicationController
 
         render json: response.body
     end 
+    
+    def episodedetails
+        id = params[:id]
+
+        response = Unirest.get "https://listen-api.listennotes.com/api/v2/episodes/#{id}",
+        headers:{
+            "X-ListenAPI-Key" => "b45a2e37fb084a6ebc1035393a023abb",
+        }  
+
+        render json: response.body
+    end
+    
+    def searchpodcasts
+        query = params[:query]
+
+        response = Unirest.get "https://listen-api.listennotes.com/api/v2/search?q=#{query}&sort_by_date=0&type=podcast&only_in=title%2Cdescription&language=English&safe_mode=1",
+        headers:{
+            "X-ListenAPI-Key" => "b45a2e37fb084a6ebc1035393a023abb",
+        }  
+
+        render json: response.body
+    end 
+
+    def podcastdetails
+        id = params[:id]
+
+        response = Unirest.get "https://listen-api.listennotes.com/api/v2/podcasts/#{id}?sort=recent_first",
+        headers:{
+            "X-ListenAPI-Key" => "b45a2e37fb084a6ebc1035393a023abb",
+        }  
+
+        render json: response.body
+    end 
 end
 
-# "https://listen-api.listennotes.com/api/v2/search?q=#{query}&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&published_before=1390190241000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/podcast', to: "podcasts#test"
   post '/search', to: "listen_notes#search"
+  post '/episodedetails', to: "listen_notes#episodedetails"
+  post '/searchpodcasts', to: "listen_notes#searchpodcasts"
+  post '/podcastdetails', to: "listen_notes#podcastdetails"
   post '/login', to: "users#login"
   post '/signup', to: "users#signup"
   post '/episode', to: "episodes#episode"
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   post '/episodes/destroy', to: "episodes#destroy"
   post '/playlists/destroy', to: "playlists#destroy"
   post '/playlists/update', to: "playlists#update"
+
 end
